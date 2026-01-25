b2e536825b1c95c620964283716f3630652d9906
Dinh Hoang Phi <phibeo345@gmail.com>
2026-01-25 15:16:30 +0700

    feat: cart remove test

--- cypress-exercise/cypress/e2e/cart_spec.cy.js
+++ cypress-exercise/cypress/e2e/cart_spec.cy.js
@@ -6,14 +6,22 @@ describe('Cart Test', () => {
     cy.get('#login-button').click();
   });
 
+  it('Should sort products by price low to high', () => {
+  cy.get('.product_sort_container').select('lohi');
+  cy.get('.inventory_item_price').first().should('have.text', '$7.99');
+  });
+
   it('Should add a product to the cart', () => {
     cy.get('.inventory_item').first().find('.btn_inventory').click();
     cy.get('.shopping_cart_badge').should('have.text', '1');
   });
 
-  it('Should sort products by price low to high', () => {
-  cy.get('.product_sort_container').select('lohi');
-  cy.get('.inventory_item_price').first().should('have.text', '$7.99');
+  it('Should remove a product from the cart', () => {
+    cy.get('.inventory_item').first().find('.btn_inventory').click();
+    cy.get('.shopping_cart_badge').should('have.text', '1');
+    cy.get('.shopping_cart_link').click();
+    cy.get('.cart_item').first().find('.cart_button').click();
+    cy.get('.shopping_cart_badge').should('not.exist');
   });
 });
 
